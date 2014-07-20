8f436d1546f995bb370bed0f64b460763c1e930b
Hollay.Yan <yanhaoliang2007@gmail.com>
2014-07-21 00:30:39 +0800

    proxy.ini

--- commands/crawler/proxy.py
+++ commands/crawler/proxy.py
@@ -17,7 +17,7 @@ class ProxyFactory():
     proxy_str = ''
     config = None
 
-    def __init__(self, init='proxy.ini'):
+    def __init__(self, init='/usr/local/etc/proxy.ini'):
         self.ini = init
         self.reload()
 
