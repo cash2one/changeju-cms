1d35309740e998bf6da69a53f2c2e6d531b38eae
Hollay.Yan <yanhaoliang2007@gmail.com>
2014-07-26 00:01:40 +0800

    修改数据库名称

--- scripts/crawler/homelinktoday.py
+++ scripts/crawler/homelinktoday.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # @Author: hollay
 # @Date:   2014-06-18 01:21:35
-# @Last Modified by:   hollay
-# @Last Modified time: 2014-07-12 19:20:34
+# @Last Modified by:   Hollay.Yan
+# @Last Modified time: 2014-07-24 15:36:35
 
 import MySQLdb
 from DBUtils.PooledDB import PooledDB
@@ -17,7 +17,7 @@ def run():
     conn = db_pool.connection()
     cur = conn.cursor()
 
-    SQL = '''select title, url, homelink_id, rent, house_type, building_year, pub_date, off_date, area, community_name from ds_homelink_hourly where homelink_id not in (select homelink_id from datasource_homelink_house where pub_date >= '2014-06-17');'''
+    SQL = '''select title, url, homelink_id, rent, house_type, building_year, pub_date, off_date, area, community_name from ds_homelink_hourly where homelink_id not in (select homelink_id from ds_homelink_house where pub_date >= '2014-06-17');'''
 
     cur.execute(SQL)
 
@@ -30,7 +30,7 @@ def run():
 
     # return
 
-    NSQL = SQL = '''INSERT INTO datasource_homelink_house (title, url, homelink_id, rent, house_type, building_year, pub_date, off_date, area, community_name) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    NSQL = SQL = '''INSERT INTO ds_homelink_house (title, url, homelink_id, rent, house_type, building_year, pub_date, off_date, area, community_name) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
     ON DUPLICATE KEY UPDATE off_date=CURDATE()-1, last_update=NOW()'''
 
     try:
